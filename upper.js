09efed1eb67b15d01414713e5d2944b5a9951d98
Ichinose Shogo <shogo82148@gmail.com>
2012-10-03 01:41:47 +0900

    テンション上げてみた

--- upper.js
+++ upper.js
@@ -40,7 +40,7 @@ window.addEventListener('load', function() {
         var canvas = document.getElementById('canvas');
 
         var width = detection_canvas.width = canvas.width = video.width;
-        var heght = detection_canvas.height = canvas.height = video.height;
+        var height = detection_canvas.height = canvas.height = video.height;
 
         var detection_context = detection_canvas.getContext('2d');
         var context = canvas.getContext('2d');
@@ -48,24 +48,58 @@ window.addEventListener('load', function() {
         video.src = videoUrl;
         video.autoplay = true;
 
+        var sin = Math.sin(10 / 180 * Math.PI);
+        var cos = Math.cos(10 / 180 * Math.PI);
         requestAnimationFrame(function render() {
-            detection_context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            detection_context.drawImage(video, 0, 0, width, height);
+            var data1 = detection_context.getImageData(0, 0, width, height);
+            var data2 = detection_context.getImageData(0, 0, width, height);
+            var data1data = data1.data, data2data = data2.data;
             var comp = ccv.detect_objects({
                 canvas :ccv.grayscale(detection_canvas),
                 cascade : cascade,
                 interval : 5,
                 min_neighbors : 1 });
-            var i, scale = 1;
+            var i, scale = 1, e, a;
+            var x, y, dx, dy, offset, doffset;
+            var w, h, cx, cy;
+            var left, right, top, bottom;
             detection_context.lineWidth = 2;
             detection_context.strokeStyle = 'rgba(230,87,0,0.8)';
             /* draw detected area */
             for (i = 0; i < comp.length; i++) {
                 detection_context.beginPath();
-                detection_context.arc((comp[i].x + comp[i].width * 0.5) * scale, (comp[i].y + comp[i].height * 0.5) * scale,
-                        (comp[i].width + comp[i].height) * 0.25 * scale * 1.2, 0, Math.PI * 2);
+                e = comp[i];
+                detection_context.arc((e.x + e.width * 0.5) * scale, (e.y + e.height * 0.5) * scale,
+                        (e.width + e.height) * 0.25 * scale * 1.2, 0, Math.PI * 2);
                 detection_context.stroke();
+
+                //copy face image
+                a = 0.7;
+                w = e.width * 1.5;
+                h = e.height * 1.5;
+                left = (e.x - e.width*0.25) | 0; right = (e.x + e.width*1.25) | 0;
+                top = (e.y - e.height*0.25) | 0; bottom = (e.y + e.height*1.25) | 0;
+                cx = (left+right)>>1;
+                cy = (top+bottom)>>1;
+                for(y = top; y <= bottom; ++y) {
+                    for(x = left; x <= right; ++x) {
+                        a = ((x-cx)*(x-cx)/(w*w) + (y-cy)*(y-cy)/(h*h)) * 4;
+                        a = (1-a) * 0.5;
+                        if(a > 1) a = 1;
+                        if(a <= 0) continue;
+                        dx = x + (e.width>>2);
+                        dy = y;
+                        if(dx < 0 || dx >= width || dy < 0 || dy >= height) continue;
+                        offset = (y * width + x) * 4;
+                        doffset = (dy * width + dx) * 4;
+                        data2data[doffset] = data2data[doffset] * (1-a) + data1data[offset] * a;
+                        data2data[doffset + 1] = data2data[doffset + 1] * (1-a) + data1data[offset + 1] * a;
+                        data2data[doffset + 2] = data2data[doffset + 2] * (1-a) + data1data[offset + 2] * a;
+                    }
+                }
             }
+            context.putImageData(data2, 0, 0);
             requestAnimationFrame(render);
         });
     }
